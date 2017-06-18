836bd4c88e30eb10a284a09a78661e55d06c4645
m652326 <Dora.Do@macys.com>
2017-06-17 22:55:42 -0700

    remove fs

--- routes/bw.js
+++ routes/bw.js
@@ -1,20 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
-var jsonfile = require('jsonfile');
-var file = '~/routes/devTimes.txt';
-jsonfile.readFile(file, function(err, obj) {
-  console.dir(obj)
-});
 
 /* GET home page. */
 router.get('/:stepId', function(req, res, next) {
   var stepId = req.params.stepId;
   res.render('bwSteps/step' + stepId);
-});
-
-router.get('/devTimes', function(req, res, next) {
-  res.send('respond with a resource');
 });
 
 module.exports = router;
